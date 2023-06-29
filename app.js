10032b7a3f41bed999dbfa4bc6ae3e03e0d6c58a
Sanjay Kumar <sanjay.11801603@gmail.com>
2023-06-29 20:56:57 +0530

    Changes for route

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const { MongoClient, ServerApiVersion } = require('mongodb')
-
-const uri = ""
-
 const app = express()
 
+const uri = ""
 mongoose.connect(uri)
 const con = mongoose.connection
 
 con.on('open', function(){
     console.log('connected ....');
+})
+
+app.use(express.json())
+
+const alienRouter = require('./routes/aliens')
+app.use('/aliens', alienRouter)
+
+app.listen(9000, () => {
+    console.log('Server listening on port 9000')
 })
 
 
