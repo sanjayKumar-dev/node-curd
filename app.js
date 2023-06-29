f12f293cfe81bd4a92b0cb22cb986b5449d5b6a2
Sanjay Kumar <sanjay.11801603@gmail.com>
2023-06-29 20:58:03 +0530

    Changes for route

--- app.js
+++ app.js
@@ -19,30 +19,3 @@ app.use('/aliens', alienRouter)
 app.listen(9000, () => {
     console.log('Server listening on port 9000')
 })
-
-
-
-// const client = new MongoClient(uri, {
-//     serverApi: {
-//       version: ServerApiVersion.v1,
-//       strict: true,
-//       deprecationErrors: true,
-//     }
-//   });
-//   async function run() {
-//     try {
-//       // Connect the client to the server	(optional starting in v4.7)
-//       await client.connect()
-//       // Send a ping to confirm a successful connection
-//       await client.db("admin").command({ ping: 1 })
-//       console.log("Pinged your deployment. You successfully connected to MongoDB!")
-//     } catch (err) {
-//         console.log('Find the Error',err)
-//     }
-//     // finally {
-//     //   // Ensures that the client will close when you finish/error
-//     //   await client.close()
-//     //   console.log('Connection closed');
-//     // }
-//   }
-//   run().catch(console.dir)
